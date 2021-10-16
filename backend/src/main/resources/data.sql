6bbeaa68c65b06ced81001fa76f0aa82b3758056
lorivalSouza <lorivalnew@gmail.com>
2021-10-16 17:16:40 -0300

    Class Offer an class Notification

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -15,3 +15,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://image.freepik.com/vetores-gratis/conceito-de-tutoriais-online_52683-37480.jpg', 'https://media.istockphoto.com/vectors/online-course-line-icon-e-learning-and-education-distance-learn-sign-vector-id924353216');
 
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-07-13T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-13T03:00:00Z', 1);
