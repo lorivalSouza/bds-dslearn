c159c78eaab8fb7b5e530c79c975a8c7c17d571b
lorivalSouza <lorivalnew@gmail.com>
2021-10-18 11:31:04 -0300

    Fix Offer to Topics

--- backend/src/main/java/com/devlps/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devlps/dslearnbds/entities/User.java
@@ -83,10 +83,6 @@ public class User implements Serializable {
 
 	public Set<Role> getRoles() {
 		return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
 	}	
 
 	public List<Notification> getNotifications() {
